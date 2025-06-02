f56f821cb29e103f75b8023909e067b1da82da92
MrFlaxKale <mrflaxkale@gmail.com>
2025-06-03 00:13:40 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-   const config = {
-       PEXELS_API_KEY: '8FAzaTTOUk5X0mXZAR5mZOh2KJuhHPEQdb4cLizXL4oi0upoWU4LQN7p'
-   };
+const config = {
+    PEXELS_API_KEY: '8FAzaTTOUk5X0mXZAR5mZOh2KJuhHPEQdb4cLizXL4oi0upoWU4LQN7p'
+};
